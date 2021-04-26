27aeca11073e23ba762d5cb984f553242fea9b3f
FilipeA <filiperamosalves@gmail.com>
2021-04-26 16:19:16 +0100

    added: Dagger as dependency Injection

---
+++ app/src/main/java/org/openobservatory/ooniprobe/di/Components/ActivityComponent.java
@@ -0,0 +1,11 @@
+package org.openobservatory.ooniprobe.di.Components;
+
+import org.openobservatory.ooniprobe.di.ApplicationModule;
+import org.openobservatory.ooniprobe.di.annotations.PerActivity;
+
+import dagger.Subcomponent;
+
+@PerActivity
+@Subcomponent()
+public interface ActivityComponent {
+}
---
+++ app/src/main/java/org/openobservatory/ooniprobe/di/Components/ServiceComponent.java
@@ -0,0 +1,11 @@
+package org.openobservatory.ooniprobe.di.Components;
+
+import org.openobservatory.ooniprobe.di.ApplicationModule;
+import org.openobservatory.ooniprobe.di.annotations.PerService;
+
+import dagger.Subcomponent;
+
+@PerService
+@Subcomponent()
+public interface ServiceComponent {
+}

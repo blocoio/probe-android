c5c24518c62ac9873dd03564a89fdb0881daacb2
FilipeA <filiperamosalves@gmail.com>
2021-04-26 17:26:33 +0100

    fixed: issue with Dagger in Unit Test

--- app/src/test/java/org/openobservatory/ooniprobe/common/ApplicationTest.java
+++ app/src/test/java/org/openobservatory/ooniprobe/common/ApplicationTest.java
@@ -5,14 +5,26 @@ import androidx.test.filters.SmallTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openobservatory.ooniprobe.RobolectricAbstractTest;
+import org.openobservatory.ooniprobe.di.TestAppComponent;
+import org.openobservatory.ooniprobe.di.TestApplication;
 import org.robolectric.RobolectricTestRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @SmallTest
 public class ApplicationTest extends RobolectricAbstractTest {
     @Test
     public void packageName() {
         assertEquals("org.openobservatory.ooniprobe.dev", a.getPackageName());
+    }
+
+    @Test
+    public void testApp() {
+        assertTrue(a instanceof TestApplication);
+    }
+    @Test
+    public void component() {
+        assertTrue(a.component instanceof TestAppComponent);
     }
 }

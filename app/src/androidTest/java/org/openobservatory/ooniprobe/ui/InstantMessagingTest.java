e0fccd10e1a93c7073bbfaabbba0cb8f0481185e
FilipeA <filiperamosalves@gmail.com>
2021-04-29 15:17:34 +0100

    added: whatsApp tests && FormattingUtils

--- app/src/androidTest/java/org/openobservatory/ooniprobe/ui/InstantMessagingTest.java
+++ app/src/androidTest/java/org/openobservatory/ooniprobe/ui/InstantMessagingTest.java
@@ -2,7 +2,6 @@ package org.openobservatory.ooniprobe.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.format.DateFormat;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
@@ -13,18 +12,18 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openobservatory.ooniprobe.DatabaseUtils;
+import org.openobservatory.ooniprobe.FormattingUtils;
 import org.openobservatory.ooniprobe.R;
 import org.openobservatory.ooniprobe.ResultFactory;
 import org.openobservatory.ooniprobe.activity.ResultDetailActivity;
+import org.openobservatory.ooniprobe.model.database.Measurement;
 import org.openobservatory.ooniprobe.model.database.Result;
 import org.openobservatory.ooniprobe.test.suite.InstantMessagingSuite;
-
-import java.text.DecimalFormat;
-import java.util.Locale;
 
 import tools.fastlane.screengrab.locale.LocaleTestRule;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.swipeLeft;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -59,13 +58,6 @@ public class InstantMessagingTest {
         // Arrange
         Result testResult = ResultFactory.createAndSave(new InstantMessagingSuite());
 
-        String formattedStartTime = DateFormat.format(
-                DateFormat.getBestDateTimePattern(Locale.getDefault(), "yMdHm"),
-                testResult.start_time
-        ).toString();
-
-        String formattedRunTime = new DecimalFormat("#0.00").format(testResult.getRuntime());
-
         // Act / Assert
         launchActivity(testResult.id);
 
@@ -76,16 +68,68 @@ public class InstantMessagingTest {
 
         // Page 2
         onView(withId(R.id.pager)).perform(swipeLeft());
-        onView(withId(R.id.startTime)).check(matches(withText(formattedStartTime)));
+        onView(withId(R.id.startTime)).check(matches(withText(FormattingUtils.formatStartTime(testResult.start_time))));
         onView(withId(R.id.download)).check(matches(withText(testResult.getFormattedDataUsageDown())));
         onView(withId(R.id.upload)).check(matches(withText(testResult.getFormattedDataUsageUp())));
-        onView(withId(R.id.runtime)).check(matches(withText(formattedRunTime)));
+        onView(withId(R.id.runtime)).check(matches(withText(FormattingUtils.formatRunTime(testResult.getRuntime()))));
 
         // Page 3
         onView(withId(R.id.pager)).perform(swipeLeft());
         onView(withText(testResult.network.country_code)).check(matches(isDisplayed()));
         onView(withText(containsString(testResult.network.network_name))).check(matches(isDisplayed()));
         onView(withText(containsString(testResult.network.asn))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSuccessWhatsApp() {
+        // Arrange
+        Result testResult = ResultFactory.createAndSave(new InstantMessagingSuite());
+        Measurement measurement = testResult.getMeasurement("whatsapp");
+
+        // Act
+        launchActivity(testResult.id);
+
+        // Assert
+        onView(withText("WhatsApp Test"))
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        onView(withId(R.id.outcome)).check(matches(withText("Working")));
+        onView(withId(R.id.application)).check(matches(withText("OK")));
+        onView(withId(R.id.webApp)).check(matches(withText("OK")));
+        onView(withId(R.id.registrations)).check(matches(withText("OK")));
+
+        onView(withId(R.id.startTime)).check(matches(withText(FormattingUtils.formatStartTime(measurement.start_time))));
+        onView(withId(R.id.runtime)).check(matches(withText(FormattingUtils.formatRunTime(measurement.runtime))));
+        onView(withId(R.id.country)).check(matches(withText(testResult.network.country_code)));
+        onView(withId(R.id.networkName)).check(matches(withText(testResult.network.network_name)));
+        onView(withId(R.id.networkDetail)).check(matches(withText(containsString(testResult.network.asn))));
+    }
+
+    @Test
+    public void testBlockedWhatsApp() {
+        // Arrange
+        Result testResult = ResultFactory.createAndSave(new InstantMessagingSuite(), 0, 4);
+        Measurement measurement = testResult.getMeasurement("whatsapp");
+
+        // Act
+        launchActivity(testResult.id);
+
+        // Assert
+        onView(withText("WhatsApp Test"))
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        onView(withId(R.id.outcome)).check(matches(withText("Likely blocked")));
+        onView(withId(R.id.application)).check(matches(withText("Failed")));
+        onView(withId(R.id.webApp)).check(matches(withText("Failed")));
+        onView(withId(R.id.registrations)).check(matches(withText("Failed")));
+
+        onView(withId(R.id.startTime)).check(matches(withText(FormattingUtils.formatStartTime(measurement.start_time))));
+        onView(withId(R.id.runtime)).check(matches(withText(FormattingUtils.formatRunTime(measurement.runtime))));
+        onView(withId(R.id.country)).check(matches(withText(testResult.network.country_code)));
+        onView(withId(R.id.networkName)).check(matches(withText(testResult.network.network_name)));
+        onView(withId(R.id.networkDetail)).check(matches(withText(containsString(testResult.network.asn))));
     }
 
 }
